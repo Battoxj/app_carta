7e500d657c90f4058e293574c4d44fd2e9fe43b1
ProgJvb <prog.jvb@gmail.com>
2020-10-18 17:10:58 -0400

    Updates

---
+++ precache-manifest.bd25a37f47f38720a990b644f942b4a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f2e49c27a03e4ab5f698141b9d8ec4e",
+    "url": "/app_carta/index.html"
+  },
+  {
+    "revision": "546870c2937da102fd9d",
+    "url": "/app_carta/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "5de1ee0e531958b500e2",
+    "url": "/app_carta/static/css/main.4d0feb5a.chunk.css"
+  },
+  {
+    "revision": "546870c2937da102fd9d",
+    "url": "/app_carta/static/js/2.d97e7082.chunk.js"
+  },
+  {
+    "revision": "5de1ee0e531958b500e2",
+    "url": "/app_carta/static/js/main.90ef8b79.chunk.js"
+  },
+  {
+    "revision": "c04f36765ce0cd01592b",
+    "url": "/app_carta/static/js/runtime~main.f2918d00.js"
+  },
+  {
+    "revision": "e5a4809b920e17ebc8b64f6d6e7d30f8",
+    "url": "/app_carta/static/media/cuantos.e5a4809b.png"
+  },
+  {
+    "revision": "e72b47108972e302d538f92233b2943d",
+    "url": "/app_carta/static/media/gohan.e72b4710.png"
+  }
+]);
