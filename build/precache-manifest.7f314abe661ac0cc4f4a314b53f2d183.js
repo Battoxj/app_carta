5851ffceec344b2621434e19d8dd46d0ba446024
ProgJvb <prog.jvb@gmail.com>
2020-10-19 17:01:47 -0400

    Cname modificado

--- build/precache-manifest.7f314abe661ac0cc4f4a314b53f2d183.js
+++ build/precache-manifest.7f314abe661ac0cc4f4a314b53f2d183.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7afe10dbb591c052c27e89574434fe44",
+    "revision": "7e2764b5f936a2f4a7b7de9fef1c2179",
     "url": "/app_carta/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/app_carta/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "17d153d2a146927bc304",
+    "revision": "497d7deda4e58f458105",
     "url": "/app_carta/static/css/main.4d0feb5a.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/app_carta/static/js/2.a92dc50a.chunk.js"
   },
   {
-    "revision": "17d153d2a146927bc304",
-    "url": "/app_carta/static/js/main.0887f125.chunk.js"
+    "revision": "497d7deda4e58f458105",
+    "url": "/app_carta/static/js/main.dd6d3d15.chunk.js"
   },
   {
     "revision": "c04f36765ce0cd01592b",
@@ -26,6 +26,14 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "e5a4809b920e17ebc8b64f6d6e7d30f8",
     "url": "/app_carta/static/media/cuantos.e5a4809b.png"
+  },
+  {
+    "revision": "f69ca570f0a142b7200ab4d6d20a836e",
+    "url": "/app_carta/static/media/cuantos.f69ca570.png"
+  },
+  {
+    "revision": "3d2359a87ea5077c5ac5bb2b58a5d143",
+    "url": "/app_carta/static/media/gohan.3d2359a8.png"
   },
   {
     "revision": "e72b47108972e302d538f92233b2943d",
